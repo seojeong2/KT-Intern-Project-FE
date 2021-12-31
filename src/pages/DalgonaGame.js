fa34c23e0fb95bf8ca8f0d78fb9f1e79a8f62f57
Suyeon Kim <amelia@Suyeonui-MacBookAir.local>
2021-12-31 15:35:39 +0900

    connect backend

--- src/pages/DalgonaGame.js
+++ src/pages/DalgonaGame.js
@@ -1,7 +1,8 @@
 import { Button, Grid, makeStyles, Paper } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import squidManager from "../img/manager.png";
+import axios from 'axios'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,18 +20,45 @@ const useStyles = makeStyles((theme) => ({
 const DalgonaGame = () => {
   const classes = useStyles();
   const navigate = useNavigate();
+  const [data,setData]=useState(0);
+  const [result,setResult]=useState('');
 
+  useEffect(()=>{
+    ff()
+  },[data])
+
+  const ff =async()=>{
+
+    await axios({
+      method:'get',
+      url:'http://127.0.0.1:5000/rr',
+      headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}   
+    }).then((response)=>{
+      console.log(response)
+      setResult(response.data)
+    }).catch((error)=>{
+      console.log('안돼!')
+    })
+  }
+  setInterval(function(){
+    setData(data+1)
+    if(result===1){
+      clearInterval()
+    }
+  },4000)
+ 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={5}>
           <Paper className={classes.paper}>
-          <img src="http://127.0.0.1:5000/video_feed" max_width="100%" />
+          <img src="http://127.0.0.1:5000/video_feed" width="100%"/>
+          {result ===0? <img src="http://127.0.0.1:5000/video_feed" width="100%"/>:<h1>끝!</h1>}
           </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>
-          <img src="http://127.0.0.1:5000/video_feed1" max_width="100%"/>
+          <img src="http://127.0.0.1:5000/video_feed1" width="100%"/>
           </Paper>
         </Grid>
         <Grid item xs={3}>
