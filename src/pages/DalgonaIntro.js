b07ef964e5137283780b6e981980d064a8948a57
Suyeon Kim <amelia@Suyeonui-MacBookAir.local>
2022-01-04 16:05:05 +0900

    add score

--- src/pages/DalgonaIntro.js
+++ src/pages/DalgonaIntro.js
@@ -7,7 +7,7 @@ const DalgonaIntro = () => {
       <Manual
         props={"/dalgonagame"}
         text={
-          "두 손가락으로 시작점을 잡고, 한 손가락으로 달고나를 깨보자! 벗어나면 죽음뿐"
+          "두 손가락으로 시작점을 잡고, 한 손가락으로 달고나를 깨보자!"
         }
       />
     </div>
